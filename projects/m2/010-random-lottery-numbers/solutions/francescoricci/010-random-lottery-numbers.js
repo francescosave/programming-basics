28798c2298af980ed9e7c5907b0dc63298aaf158
Francesco Ricci <francescosav.ricci@libero.it>
2022-03-26 10:52:59 +0100

    feat:Completed exercise m2-010-random-lottery-numbers

---
+++ projects/m2/010-random-lottery-numbers/solutions/francescoricci/010-random-lottery-numbers.js
@@ -0,0 +1,40 @@
+// exercise 010 random lottery numbers by Francesco Ricci
+
+function getRandomInt(min, max) {
+  /*
+  This function generates an random number between min and max value. 
+  */
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+function getLotteryNumbers() {
+  /*
+  This function generates an array with six random numbers and teturns sorted.
+  */
+
+  let list = [];
+  let count = 0;
+
+  while (list.length < 6) {
+
+    let numberRandom = getRandomInt(1, 49);
+    count++;
+    if (!list.includes(numberRandom)) {
+      list.push(numberRandom);
+    }
+  }
+
+  list.sort(function (a, b) { return a - b });
+  
+  return list
+}
+
+console.log(getLotteryNumbers().toString());
+
+
+
